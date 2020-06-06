bec28fd4ff97417803f8b9358e127db26f44469e
arunvemireddy <vemireddyarun0701@gmail.com>
2020-06-06 22:13:15 +0530

    On branch master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { SampleComponent } from './sample/sample.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   imports: [
@@ -51,7 +52,8 @@ import { SampleComponent } from './sample/sample.component';
     MessagesComponent,
     HeroSearchComponent,
     DataTableComponent,
-    SampleComponent
+    SampleComponent,
+    DemoComponent
   ],
   bootstrap: [ AppComponent ]
 })
